695e707fbd64aa216f588f45cc3b2bb1c0974ca0
eugenii <eugenii.cher@gmail.com>
2024-04-07 19:32:19 +0300

    eee

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -5,4 +5,8 @@ from django import forms
 class BirthdayForm(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(required=False)
-    birthday = forms.DateField()
+    birthday = forms.DateField(
+        label='Дата рождения',
+        # Указываем, что виджет для ввода даты должен быть с типом date.
+        widget=forms.DateInput(attrs={'type': 'date'})
+    ) 
